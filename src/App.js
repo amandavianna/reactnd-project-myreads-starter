e87f93b0369f9e6b14c12f0b04146ebbf5789d78
Amanda Vianna <amanda_vianna@hotmail.com>
2017-11-02 20:51:21 -0200

    Use componentDidMount to fetch books from the server

--- src/App.js
+++ src/App.js
@@ -4,18 +4,32 @@ import { Route } from 'react-router-dom'
 import SearchBooks from './components/SearchBooks'
 import ListBooks from './components/ListBooks'
 
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books })
+    })
+  }
+
   render() {
+    const { books } = this.state
+
     return (
       <div className="app">
         <Route exact path="/search" render={() => (
           <SearchBooks />
         )}/>
         <Route exact path="/" render={() => (
-          <ListBooks />
+          <ListBooks
+            books={books}
+          />
         )}/>
       </div>
     )
