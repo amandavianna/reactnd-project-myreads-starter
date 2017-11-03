812a8f172a74b9533716f718182e11b1b1aed34f
Amanda Vianna <amanda_vianna@hotmail.com>
2017-11-02 22:39:24 -0200

    Change Bookshelf to function component

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import Book from './Book'
 
-class Bookshelf extends Component {
-  render() {
-    const { title, groupBooks } = this.props
+const Bookshelf = (props) => {
+  const { title, groupBooks } = props
 
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {groupBooks.map(book => (
-              <li key={book.id}>
-                <Book
-                  book={book}
-                />
-              </li>
-            ))}
-          </ol>
-        </div>
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {groupBooks.map(book => (
+            <li key={book.id}>
+              <Book
+                book={book}
+              />
+            </li>
+          ))}
+        </ol>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 Bookshelf.propTypes = {
