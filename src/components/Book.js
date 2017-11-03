fe86129cb688a166810129d9686064604e4f50dd
Amanda Vianna <amanda_vianna@hotmail.com>
2017-11-03 01:02:42 -0200

    Add book cover size dynamically

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,27 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import BookShelfChanger from './BookShelfChanger'
 
-const Book = (props) => {
-    const { book, updateBookShelf } = props
+class Book extends Component {
+  state = {
+    coverHeight: 0,
+    coverWidth: 0
+  }
 
-  return (
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
-        <BookShelfChanger
-          bookInShelf={book}
-          updateBookShelf={updateBookShelf}
-        />
+  loadBookCover(bookCover) {
+    let bookImage = new Image()
+    bookImage.src = bookCover
+
+    bookImage.onload = (e) => {
+      this.setState({
+        coverHeight: bookImage.height,
+        coverWidth: bookImage.width
+      })
+    }
+  }
+
+  componentDidMount() {
+    this.loadBookCover(this.props.book.imageLinks.thumbnail)
+  }
+
+  render() {
+    const { book, updateBookShelf } = this.props
+    const { coverHeight, coverWidth } = this.state
+
+    return (
+      <div className="book">
+        <div className="book-top">
+        <div className="book-cover" style={{ width: coverWidth, height: coverHeight, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+          <BookShelfChanger
+            bookInShelf={book}
+            updateBookShelf={updateBookShelf}
+          />
+        </div>
+        <div className="book-title">{book.title}</div>
+        {book.authors && book.authors.length && (
+          book.authors.map((author, index) => (
+          <div key={index} className="book-authors">{author}</div>
+        )))}
       </div>
-      <div className="book-title">{book.title}</div>
-      {book.authors && book.authors.length && (
-        book.authors.map((author, index) => (
-        <div key={index} className="book-authors">{author}</div>
-      )))}
-    </div>
-  )
+    )
+  }
 }
 
 Book.propTypes = {
