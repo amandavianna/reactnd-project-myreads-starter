4630a57576f9c42b7952efce99fb0d11230aa37e
Amanda Vianna <amanda_vianna@hotmail.com>
2017-11-02 22:55:05 -0200

    Add BookShelfChanger component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import BookShelfChanger from './BookShelfChanger'
+
 const Book = (props) => {
     const { book } = props
 
@@ -8,15 +10,9 @@ const Book = (props) => {
     <div className="book">
       <div className="book-top">
         <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
-        <div className="book-shelf-changer">
-          <select>
-            <option value="none" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
-        </div>
+        <BookShelfChanger
+          bookInShelf={book}
+        />
       </div>
       <div className="book-title">{book.title}</div>
       {book.authors && book.authors.length && (
