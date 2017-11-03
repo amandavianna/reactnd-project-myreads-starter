69f9bd6b1958f93680cb88e107a660609da81da9
Amanda Vianna <amanda_vianna@hotmail.com>
2017-11-02 22:36:47 -0200

    Add PropTypes package to verify Bookshelf props

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 import Book from './Book'
 
 class Bookshelf extends Component {
   render() {
+    const { title, groupBooks } = this.props
+
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
+        <h2 className="bookshelf-title">{title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <Book />
-            </li>
+            {groupBooks.map(book => (
+              <li key={book.id}>
+                <Book
+                  book={book}
+                />
+              </li>
+            ))}
           </ol>
         </div>
       </div>
     )
   }
+}
+
+Bookshelf.propTypes = {
+  title: PropTypes.string.isRequired,
+  groupBooks: PropTypes.array.isRequired
 }
 
 export default Bookshelf
